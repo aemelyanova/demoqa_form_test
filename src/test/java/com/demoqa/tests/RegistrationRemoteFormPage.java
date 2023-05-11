4778864bd5d2727742c8c1af584d12aea62cf2a0
Anastasia Emelyanova <aemelyanova333@gmail.com>
2023-05-11 16:39:22 +0300

    added remote tests with selenoid

--- src/test/java/com/demoqa/tests/RegistrationRemoteFormPage.java
+++ src/test/java/com/demoqa/tests/RegistrationRemoteFormPage.java
@@ -6,6 +6,8 @@ import com.demoqa.data.Subjects;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
 
 public class RegistrationRemoteFormPage extends RemoteTestBase {
 
@@ -26,34 +28,55 @@ public class RegistrationRemoteFormPage extends RemoteTestBase {
     @Tag("remote")
     public void fillInRegistrationForm() {
 
-        registrationPage.openPage()
-                .removeFooter()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(email)
-                .setGender(genders)
-                .setUserNumber(phoneNumber)
-                .setBirthDate(dateBirthday[0], dateBirthday[1], dateBirthday[2])
-                .setSubject(subjects)
-                .setHobbies(hobbies)
-                .setPicture(picture)
-                .setAddress(address)
-                .setState(state)
-                .setCity(city)
-                .setSubmit();
+        step("Открываем страницу", () -> {
+            registrationPage.openPage()
+                    .removeFooter();
+        });
+        step("Ввести имя", () -> {
+            registrationPage.setFirstName(firstName);
+        });
+        step("Ввести фамилию", () -> {
+            registrationPage.setLastName(lastName);
+        });
+        step("Ввод email", () -> {
+            registrationPage.setLastName(lastName);
+        });
+        step("Выбор пола", () -> {
+            registrationPage.setGender(genders);
+        });
+        step("Ввести номер телефона", () -> {
+            registrationPage.setUserNumber(phoneNumber);
+        });
+        step("Ввести дату рождения", () -> {
+            registrationPage.setBirthDate(dateBirthday[0], dateBirthday[1], dateBirthday[2]);
+        });
+        step("Выбрать предмет", () -> {
+            registrationPage.setSubject(subjects);
+        });
+        step("Загрузить фото", () -> {
+            registrationPage.setPicture(picture);
+        });
+        step("Выбрать штат и город", () -> {
+            registrationPage.setState(state)
+                    .setCity(city);
+        });
+        step("Отправить форму", () -> {
+            registrationPage.setSubmit();
+        });
+        step("Проверить правильность заполнения полей", () -> {
+            registrationPage.verifyRegistrationResultsModalAppears()
+                    .verifyResult("Student Name", String.format("%s %s", firstName, lastName))
+                    .verifyResult("Student Email", email)
+                    .verifyResult("Gender", genders.toString())
+                    .verifyResult("Mobile", phoneNumber)
+                    .verifyResult("Date of Birth", String.format("%s %s,%s", dateBirthday[0], dateBirthday[1], dateBirthday[2]))
+                    .verifyResult("Subjects", subjects.toString())
+                    .verifyResult("Hobbies", hobbies.toString())
+                    .verifyResult("Picture", picture)
+                    .verifyResult("Address", address)
+                    .verifyResult("State and City", String.format("%s %s", state, city));
 
-        registrationPage.verifyRegistrationResultsModalAppears()
-                .verifyResult("Student Name", String.format("%s %s", firstName, lastName))
-                .verifyResult("Student Email", email)
-                .verifyResult("Gender", genders.toString())
-                .verifyResult("Mobile", phoneNumber)
-                .verifyResult("Date of Birth", String.format("%s %s,%s", dateBirthday[0], dateBirthday[1], dateBirthday[2]))
-                .verifyResult("Subjects", subjects.toString())
-                .verifyResult("Hobbies", hobbies.toString())
-                .verifyResult("Picture", picture)
-                .verifyResult("Address", address)
-                .verifyResult("State and City", String.format("%s %s", state, city));
-
+        });
     }
 }
 
--- src/test/java/com/demoqa/tests/RemoteTestBase.java
+++ src/test/java/com/demoqa/tests/RemoteTestBase.java
@@ -1,9 +1,12 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.demoqa.pages.RegistrationPage;
 import com.demoqa.utils.RandomUtils;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -24,6 +27,15 @@ public class RemoteTestBase {
         ));
 
         Configuration.browserCapabilities = capabilities;
+
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
     }
 }
+
+
 
